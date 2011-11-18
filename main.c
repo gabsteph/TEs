c5d1f5c8134eb1e9e7f70aedc2f2cc174dd71a1f
gabsteph <gabriel.steph@gmail.com>
2011-11-18 15:39:19 +0100

    adding display function for the struct and testing it

--- main.c
+++ main.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
+typedef struct BLAH {
+        int cnt;
+        int cnt2;
+        char c;
+} BLAH;
+int display_blah(BLAH a_blah) {
+        printf("struct is : %u, %u and %c\r\n", a_blah.cnt, a_blah.cnt2, a_blah.c);
+        return(0);
+}
 int main(int argc, char *argv[])
 {
         int i;
+        BLAH one;
         for(i = 0; i < 5; i++) {
                 if (argc >= 1)
                         printf("you entered something : %s\r\n", argv[1]);
         }
+        one.cnt = 1;
+        one.cnt2 = one.cnt + 1;
+        one.c = ‘a’;
+        display_blah(one);
         return (0);
 }
